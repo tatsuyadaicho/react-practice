5e43198d66c3598872d48934d0811b280aabe4d3
t_daicho <daicho.t@gmail.com>
2016-05-04 23:49:23 +0900

    propsを使う

--- src/js/comment_list.js
+++ src/js/comment_list.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Comment from './comment';
 
 export default class CommentList extends React.Component {
     render() {
         return (
             <div className="commentList">
-                comment list
+                <Comment author="Pete Hunt">comment one</Comment>
+                <Comment author="Jordan Walke">comment two</Comment>
             </div>
         );
     }
